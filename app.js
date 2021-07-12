0b19e60bc5611a2b5df8f76e26270c812a40928c
aarti0414 <apwaraikar04033@gmail.com>
2021-07-12 23:01:38 +0530

    Association Added

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product=require('./models/product');
+const User=require('./models/user');
+
 
 const app = express();
 
@@ -20,12 +23,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-sequelize.sync()
-.then(result =>{
+Product.belongsTo(User,{constraints : true, onDelete:'CASCADE'})
+User.hasMany(Product);
+
+sequelize
+  .sync({force: true})
+  .then(result =>{
     // console.log(result)
     app.listen(3000);
-})
-.catch(err=>{
+  })
+  .catch(err=>{
     console.log(err);
 })
 
