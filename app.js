caf7fb972620741cb618e74e6033b95e717ff53c
aarti0414 <apwaraikar04033@gmail.com>
2021-07-15 21:01:58 +0530

    connect Frontend to backend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
@@ -21,6 +22,7 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors()); 
 
 app.use((req,res,next)=>{
     User.findByPk(1)
